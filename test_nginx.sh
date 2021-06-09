9d5972499fec4637d6dd60a1eeab8c77b77c38cc
dima <dimitrioslisenko@gmail.com>
2021-06-09 10:40:27 +0100

    Test mruby integration

--- test_nginx.sh
+++ test_nginx.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Checking NGINX version"
 nginx -V
+
+echo "Testing NGINX configuration"
 nginx -t
+
+echo "Starting up NGINX"
 nginx
+
+echo "Testing NGINX process is up"
 ps aux | grep '[n]ginx'
+
+echo "Testing Passenger process is up"
 curl localhost:80
 ps aux | grep '[P]assenger'
+
+echo "Testing Passenger passenger-config executable"
 passenger-config --root
+
+echo "Testing Passenger passenger-status executable"
 passenger-status
+
+echo "Testing NGINX mruby module"
+curl localhost:80/mruby | grep "mruby success"
