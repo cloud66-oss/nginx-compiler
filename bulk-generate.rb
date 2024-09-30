ecd7f8fa8313296c6d6067dd5c63f152caff5786
dima <dima@cloud66.com>
2024-09-30 14:17:47 +0100

    Add Ubuntu 24.04 to the list of OSes

--- bulk-generate.rb
+++ bulk-generate.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'date'
 RELEASE_VERSION = ARGV[0]
-OS_VERSIONS = %w(20.04 22.04)
+OS_VERSIONS = %w(20.04 22.04 24.04)
 NGINX_VERSIONS = %w(1.24.0)
 PASSENGER_VERSIONS = %w(6.0.22)
 
